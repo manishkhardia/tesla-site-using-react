70708a94f2793c63cc9f191fa854e366dccf49d1
vijendra verma <v.verma@xieno.com>
2022-02-15 01:10:37 +0530

    all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import Header from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
+}
+
+export default App;
